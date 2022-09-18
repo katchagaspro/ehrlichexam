b2ee9fb427b27e292d6ed6fa3b6f2af312074d78
katchagaspro <kat.chagas.pro@gmail.com>
2022-09-18 12:30:27 +0800

    Carousel Fade Animation
    - Set the carousel animation to fade.
    - Added texts to second and third slide.

--- pages/home/carousel.js
+++ pages/home/carousel.js
@@ -2,6 +2,42 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function HomeCarousel() {
   return (
+    <Carousel fade>
+
+      <Carousel.Item>
+        <img className="d-block w-100" src="Hero.png" alt="First slide" />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img className="d-block w-100" src="Hero.png" alt="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img className="d-block w-100" src="Hero.png" alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+    </Carousel>
+  );
+}
+
+export default HomeCarousel;
+
+
+
+/*import Carousel from 'react-bootstrap/Carousel';
+
+function HomeCarousel() {
+  return (
     <Carousel>
 
         <Carousel.Item>
@@ -25,4 +61,4 @@ function HomeCarousel() {
   );
 }
 
-export default HomeCarousel;
+export default HomeCarousel;*/
